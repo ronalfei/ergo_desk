873cb4d6d4b3077f11a5159ad25a4fce48522c09
BlackAnimal <ronalfei@qq.com>
2023-02-27 17:08:36 +0800

    init webserver

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 all:
 	source ~/.bashrc
+	go env -w GO111MODULE=off
 	go run main.go
+
+build:
+	go build main.go 
+	mv main ergo_desk
---
+++ src/webserver/web_root_handler.go
@@ -0,0 +1,14 @@
+package webserver
+
+import (
+	"github.com/ergo-services/ergo/gen"
+)
+
+type rootHandler struct {
+	gen.WebHandler
+}
+
+func (r *rootHandler) HandleRequest(process *gen.WebHandlerProcess, request gen.WebMessageRequest) gen.WebHandlerStatus {
+	request.Response.Write([]byte("Hello"))
+	return gen.WebHandlerStatusDone
+}
